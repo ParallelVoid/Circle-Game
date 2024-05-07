2a8f807bf84700274d4467c57635f034952df295
ParallelVoid <86805807+ParallelVoid@users.noreply.github.com>
2024-05-07 14:59:05 -0700

    Gameplay Added

--- src/1button/1button.py
+++ src/1button/1button.py
@@ -1,10 +1,9 @@
-import math
 import random
 import pygame
 from pygame.locals import *
-from static_circle import static_cirlce
-from moving_circle import moving_circle
 from draw_point_line import draw_point_line
+from moving_circle import moving_circle
+from static_circle import static_cirlce
 
 
 # screen = pygame.display.set_mode((64, 64))
@@ -26,9 +25,14 @@ def main():
     score = 0
     carry_on = True
     clock = pygame.time.Clock()
+    was_in_range = False
+    in_range = False
+    key = ''
 
     while carry_on:
         for event in pygame.event.get():
+            if event.type == pygame.KEYUP:
+                key=pygame.key.name(event.key)
             if event.type == pygame.QUIT:
                 carry_on = False
         screen.fill((255, 255, 255))
@@ -39,6 +43,9 @@ def main():
         else:
             diam = 2*screen.get_width()/3
 
+        if key == "escape":
+            carry_on = False
+
         static_cirlce(screen, (0, 0, 0), diam)
         static_cirlce(screen, (0, 255, 0), diam/2)
 
@@ -47,11 +54,26 @@ def main():
 
         player_pos = moving_circle(screen, (255, 0, 255), move, diam)
         if draw_point_line(screen, point, diam/2, diam/4, player_pos[0], player_pos[1], x, y):
-            score += 1
-            prev = point
-            while (prev - 40) <= point <= 40 + prev:
-                point = random.randint(0, 360)
-            dir *= -1
+            was_in_range = True
+            in_range = was_in_range
+            if key == 'space':
+                score += 1
+                prev = point
+                in_range = False
+                while (prev - 45) <= point <= 45 + prev:
+                    point = random.randint(0, 360)
+                dir *= -1
+                if dir < 0:
+                    dir += -.25
+                else:
+                    dir += .25
+
+        if not was_in_range and key == 'space':
+            carry_on = False
+        if in_range and not was_in_range:
+            carry_on = False
+        was_in_range = False
+        key = ''
 
         pygame.display.flip()
         clock.tick(60)
